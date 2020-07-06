738df64d6144c28d7c52e25b5a3e214e33bb9510
Payne-Cong <lee.pl2020@outlook.com>
2020-07-06 16:35:08 +0800

    zip test

--- src/main/scala/com/pcong/action/Test1.scala
+++ src/main/scala/com/pcong/action/Test1.scala
@@ -40,6 +40,30 @@ object Test1 {
 //    (3,4)
 //    (4,5)
 
+    val sc = spark.sparkContext
+
+    val rdd2 = sc.makeRDD(1 to 5 ,2)
+    val rdd3 = sc.makeRDD(Seq("A","B","C","D","E"),2)
+
+    /**
+     * zip : 将两个rdd 组合成 key/value 形式的 rdd ,要求 分区数 和 元素的 数量 完全一直
+     * zipPartition :  要求分区数 一致
+     *
+     * zipIndex : 将 rdd 分区的 元素 和 索引号 组合成 键值对
+     * zipWithUniqueId : 将 rdd 分区的 元素 和 一个唯一ID 组合成 键值对
+     * 唯一ID 算法如下:
+     *  每个分区中的第一个元素的唯一ID 为: 该分区索引号
+     *  每个分区中的第N个元素的唯一ID 为: 前一个元素的唯一ID 加上 该rdd 的 总的分区数
+     *
+     * zipIndex : 需要启动spark作业来进行计算
+     * zipWithUniqueId : 则不需要
+     * */
+
+    rdd2.zip(rdd3).collect().foreach(e=>println(e))
+
+    rdd3.zipWithIndex().collect().foreach(e=>println(e))
+    println()
+    rdd3.zipWithUniqueId().collect().foreach(e=>println(e))
 
   }
 
